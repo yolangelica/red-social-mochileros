b33fea28aeb1f7b7817704481cffac9df4812640
Kdangelo <kdangelof@gmail.com>
2018-01-04 16:42:03 -0300

    modificando index3

--- assets/js/app3.js
+++ assets/js/app3.js
@@ -1,26 +1,27 @@
-window.onload = function () {
-	var sendButton = document.getElementsByName("send")[0];
-	var textArea = document.getElementsByName("comment")[0];	
-  var timeLine = document.getElementsByName("newComment")[0];
-  
-  //agregando evento al botón
-	sendButton.onclick = function() {
-		if (textArea.value == "")   {
-			document.getElementsByName("send").disabled = true;
-			} else {
-			//creando elementos del nuevo comentario
-			var commentDiv = document.createElement("div");
-			var commentDate = document.createElement("span");
-			var commentContent = document.createElement("p");
-			//agregando el nuevo comentario a sus elementos
-			commentDiv.appendChild(commentDate);
-			commentDiv.appendChild(commentContent);
-			commentDiv.className = "comment";
-		
-			commentDate.textContent = new Date();
-			commentContent.textContent = textArea.value;
-			textArea.value = "";
-			timeLine.insertBefore(commentDiv, timeLine.children[0]);
-		}
-  }
+window.onload = function() {
+    /*el primero de sus elementos que retorna porque 
+    sabemos que existe al menos uno*/
+    var sendButton = document.getElementsByName("btn")[0];
+    var thinkInput = document.getElementsByName("texto")[0];
+    var timeLine = document.getElementsByName("lineaDeTiempo")[0];
+    var cont = document.getElementById("cont");
+   
+    sendButton.onclick = function() {/*al hacer click*/
+        if (thinkInput.value == "") {/*si no escribe y da click*/
+            sendButton.disabled =false;/*desabilita boton al llegar a los 140 caracteres*/
+
+        } else {/*si escribe algo y da click*/
+            var chatDiv = document.createElement("div");/*crea div*/
+            var nameSpan = document.createElement("span");/*da nombre usuario*/
+            var dateSpan = document.createElement("span");/*fecha */
+            var chatP = document.createElement("p");/*crea parrafo*/
+            chatDiv.appendChild(nameSpan);/*en el div creado agrega nombre usuario*/
+            chatDiv.appendChild(dateSpan);/*en el div agrega fecha*/
+            chatDiv.appendChild(chatP);
+            chatDiv.className = "chat"
+           
+            chatP.textContent = thinkInput.value;
+            timeLine.insertBefore(chatDiv, timeLine.children[0]);
+        }
+    }
 }
